71d4331eb9c16db0a0036dfad0dbb6d3109f99ff
Hamtammr <mariodelycan@gmail.com>
2022-06-29 16:06:49 -0300

    Inserindo list view

--- app/src/main/java/com/example/how/Personagens.java
+++ app/src/main/java/com/example/how/Personagens.java
@@ -4,18 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.List;
+
 public class Personagens extends AppCompatActivity {
 
     // Botões - tela Personagens
 
-    Button btnSalvarChar, btnExcluirChar;
+    Button btnSalvarChar, btnExcluirChar,pesquisarChar;
     EditText nomeChar, racaChar, classeChar,idChar;
     ListView listaChar;
+    ArrayAdapter persoArrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +33,7 @@ public class Personagens extends AppCompatActivity {
         racaChar = findViewById(R.id.racaPerso);
         classeChar = findViewById(R.id.classePerso);
         idChar = findViewById((R.id.idPerso));
+        pesquisarChar = findViewById(R.id.pesquisaPerso);
 
         //Listeners
 
@@ -55,6 +61,19 @@ public class Personagens extends AppCompatActivity {
 
             }
         });
+
+       pesquisarChar.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               DBHelper dbHelper = new DBHelper(Personagens.this);
+               List<Personagem> everyPerso = dbHelper.getPersonagens();
+
+               persoArrayAdapter = new ArrayAdapter<Personagem>(Personagens.this, android.R.layout.simple_list_item_1, everyPerso);
+               listaChar.setAdapter(persoArrayAdapter);
+
+               //Toast.makeText(Personagens.this, everyPerso.toString(), Toast.LENGTH_SHORT).show();
+           }
+       });
 
         btnExcluirChar.setOnClickListener(new View.OnClickListener() {
             @Override
