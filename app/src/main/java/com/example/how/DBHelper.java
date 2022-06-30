c6c6cb46b237d467e222f99bf5381ae283fbd879
Hamtammr <mariodelycan@gmail.com>
2022-06-29 21:29:34 -0300

    Revert "Revert "Inserindo list view""
    This reverts commit d610cd618be0682002aad1c14943d7259696eec9.

--- app/src/main/java/com/example/how/DBHelper.java
+++ app/src/main/java/com/example/how/DBHelper.java
@@ -2,10 +2,14 @@ package com.example.how;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DBHelper extends SQLiteOpenHelper {
 
@@ -36,26 +40,28 @@ public class DBHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         String createTableJogador = "CREATE TABLE " + TABELA_JOGADOR + " (" + COL_IDJOGADOR + " INTEGER PRIMARY KEY AUTOINCREMENT, " + COL_NOME_JOGADOR + " TEXT, " + COL_IDADE + " INTEGER)";
-
-        db.execSQL(createTableJogador);
-
         String createTablePersonagem = "CREATE TABLE " + TABELA_PERSONAGEM + " (" + COL_IDPERSONAGEM + " INTEGER PRIMARY KEY AUTOINCREMENT, " + COL_NOME_PERSONAGEM + " TEXT, " + COL_RACA + " TEXT, " + COL_CLASSE + " TEXT)";
-
-        db.execSQL(createTablePersonagem);
-
         String createTableCampanha = "CREATE TABLE " + TABELA_CAMPANHA + " (" + COL_IDCAMPANHA + " INTEGER PRIMARY KEY AUTOINCREMENT, " + COL_NOME_CAMPANHA + " TEXT, " + COL_SISTEMA + " TEXT)";
 
+        db.execSQL(createTableJogador);
+        db.execSQL(createTablePersonagem);
         db.execSQL(createTableCampanha);
 
-
-
     }
+
 
     // Comando para atualização do banco de dados
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("DRROP TABLE IF EXISTS "+TABELA_JOGADOR);
+        db.execSQL("DRROP TABLE IF EXISTS "+TABELA_PERSONAGEM);
+        db.execSQL("DRROP TABLE IF EXISTS "+TABELA_CAMPANHA);
+        onCreate(db);
+
 
     }
+
+    // Inserção dos dados referentes ao objeto Jogador na tabela
 
     public boolean addJogador(Jogador jogador){
         SQLiteDatabase db = this.getWritableDatabase();
@@ -72,6 +78,8 @@ public class DBHelper extends SQLiteOpenHelper {
         }
     }
 
+    // Inserção dos dados referentes ao objeto Personagem na tabela
+
     public boolean addPersonagem(Personagem personagem) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues cv = new ContentValues();
@@ -86,5 +94,33 @@ public class DBHelper extends SQLiteOpenHelper {
         } else {
             return true;
         }
+    }
+
+    public List<Personagem> getPersonagens() {
+        List<Personagem> returnList = new ArrayList<>();
+
+        String queryString = "SELECT * FROM "+ TABELA_PERSONAGEM;
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(queryString, null);
+
+        if (cursor.moveToFirst()){
+
+            do {
+                int IDpersonagem = cursor.getInt(0);
+                String nomePersonagem = cursor.getString(1);
+                String classePersonagem = cursor.getString(2);
+                String racaPersonagem = cursor.getString(3);
+
+                Personagem novoPersonagem = new Personagem(IDpersonagem, nomePersonagem, classePersonagem, racaPersonagem);
+                returnList.add(novoPersonagem);
+
+            }while (cursor.moveToNext());
+
+        }
+
+        cursor.close();
+        db.close();
+
+        return  returnList;
     }
 }
